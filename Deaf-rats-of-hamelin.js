8353536802b78527b1dff663877f0f1653aac738
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-11-30 11:13:33 -0800

    Update Deaf-rats-of-hamelin.js

--- Deaf-rats-of-hamelin.js
+++ Deaf-rats-of-hamelin.js
@@ -17,3 +17,20 @@ Example:
 
 
 // Solution
+
+let countDeafRats = function(town) {
+  let deafs = 0
+  let ident = '0'
+  for (let i = 0; i < town.length; i++) {
+    if (town[i] === 'P') {
+      ident = '~'
+    }
+    if (town[i] === ident) {
+      deafs++
+    }
+    if (town[i] === '~' || town[i] === '0') {
+      i++
+    }
+  }
+  return deafs;
+}
